602a631b58375d895a63d4af0719d2fbdbca02ac
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-07-26 22:48:36 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  let upToTen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let upToTwenty = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let upToHundred = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundred = 'hundred';
+  let result = '';
+  let numberString = number.toString();
+
+  if (number === 0) {
+    return 'zero';
+  } else if (number < 10) {
+    result = upToTen[number];
+    return result;
+  } else if (number >=10 && number < 20) {
+    result = upToTwenty[number - 9];
+    return result;
+  }
 }
