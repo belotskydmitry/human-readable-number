1197d275137927a9a9fe9eb32b28428a2ea3cb45
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-08-06 14:56:37 +0300

    fix: fix task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = function toReadable (number) {
   let upToHundred = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   let hundred = 'hundred';
   let result = '';
-  let numberString = number.toString();
 
   if (number === 0) {
     return 'zero';
@@ -14,5 +13,14 @@ module.exports = function toReadable (number) {
   } else if (number >=10 && number < 20) {
     result = upToTwenty[number - 9];
     return result;
+  } else if (number >= 20 && number < 100 && number % 10 === 0) {
+    result = upToHundred[number / 10];
+    return result;
+  } else if (number > 20 && number < 100 && number % 10 !== 0) {
+    number = number.toString().split('');
+    result = upToHundred[number[0]] + ' ' + upToTen[number[1]];
+    return result;
+  } else if (number === 100) {
+    return 'one' + ' ' + hundred;
   }
 }
